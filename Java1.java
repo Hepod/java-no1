aa1fe3270cfe828f55b66fbcd1394c73158e6706
胡渣 <785933755@qq.com>
2018-04-10 15:58:12 +0800

    创建Java文件
    第一次Java文件

---
+++ Java1.java
@@ -0,0 +1,13 @@
+
+public class demo1 {
+    public static void main(String[] args){
+
+    }
+    public void run(){
+        System.out.println("Hello,MyJava!");
+    }
+    public void eat(){
+        System.out.println("eat");
+    }
+
+}
